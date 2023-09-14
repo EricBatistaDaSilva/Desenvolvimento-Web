f927090f2cb64498ee995ab55adb7a68e5018938
Eric Batista <batistaeric62@gmail.com>
2023-09-14 20:29:13 -0300

    update class

--- 4-JavaScript/class-01-data-types/script.js
+++ 4-JavaScript/class-01-data-types/script.js
@@ -1,2 +1,30 @@
-console.log("Hello World!!!");
-console.log("bye bye");
+//primitives (String, Number, Boolean...)
+
+let studant = "Eric";
+let age = "19";
+let weight = 48.8;
+
+console.log(typeof studant);
+console.log(typeof age);
+console.log(age);
+console.log(weight);
+
+let number1 = 15;
+let number2 = "5";
+
+console.log(number1 + number2);
+
+let isStudantInCvt = false;
+
+console.log(isStudantInCvt);
+console.log(typeof isStudantInCvt);
+
+console.log(10 == 10);
+console.log(10 == 20);
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 != 20);
+
+let yourName = prompt("digite seu nome: ")
+
+console.log(yourName);
