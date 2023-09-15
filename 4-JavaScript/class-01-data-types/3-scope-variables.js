f3167b6609080f8aa147ab5b62d88d6c9eca6393
Eric Batista <batistaeric62@gmail.com>
2023-09-15 20:38:10 -0300

    update class

---
+++ 4-JavaScript/class-01-data-types/3-scope-variables.js
@@ -0,0 +1,18 @@
+let num = "global";
+const PI = 3.14;
+
+console.log(num);
+console.log(PI);
+
+{
+    let num = "local"
+    const PI = 5
+
+    console.log(num);
+    console.log(PI);
+}
+
+{
+    console.log(num);
+    console.log(PI);  
+}
---
+++ 4-JavaScript/class-01-data-types/challenges/script.js
@@ -0,0 +1,11 @@
+let name = prompt("Digite seu nome: ");
+
+alert("Bem-vindo, " + name + "! Vamos fazer uma soma");
+
+let number1 = Number(prompt("Digite o primeiro número: "));
+
+let number2 = Number(prompt("Digite o segundo número: "));
+
+let addition = number1 + number2;
+
+alert(`A soma de ${number1} e ${number2} é: ${addition}`);
