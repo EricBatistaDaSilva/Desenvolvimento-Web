b595efc35b45b5d4d48ef3e4a8cf03a56afc373a
Eric Batista <batistaeric62@gmail.com>
2023-09-25 20:24:21 -0300

    add class 04 data manipulation

---
+++ 4-JavaScript/class-04-data-manipulation/string.js
@@ -0,0 +1,40 @@
+let myName = "Eric"
+
+console.log(myName.__proto__);
+console.log(myName.length);
+console.log(myName[1]);
+console.log(myName.toLocaleUpperCase());
+console.log(myName.toLocaleLowerCase());
+
+console.log(myName[1] = "R"); // string immutable
+console.log(myName);
+
+let phrase = "   Im a web developer web web   "
+
+console.log(phrase.split(" ")[0] = "WEB");
+console.log(typeof phrase);
+
+console.log(phrase.split("").reverse().join(""));
+
+console.log(phrase.replace("web", "WEB"));
+console.log(phrase.replaceAll("web", "WEB"));
+console.log(phrase);
+
+console.log(phrase.indexOf(""));
+console.log(phrase);
+console.log(phrase.trim());
+
+phrase = phrase.trim();
+console.log(phrase.substring(5));
+
+console.log(phrase.includes("web"));
+console.log("web" == "Web"); // case-sensitive
+console.log(phrase.toLocaleLowerCase().includes("web") ? "Teste Verdadeiro" : "Teste Falso");
+
+const aplication = {
+    console: function log() {
+        return "imprimir"
+    }
+}
+
+console.log();
