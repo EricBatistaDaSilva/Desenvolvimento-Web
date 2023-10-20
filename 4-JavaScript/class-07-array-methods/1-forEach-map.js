b6cbf7428e6ceb48a5bae754d9813923334d94be
Eric Batista <batistaeric62@gmail.com>
2023-10-20 19:22:38 -0300

    update forEach map

--- 4-JavaScript/class-07-array-methods/1-forEach-map.js
+++ 4-JavaScript/class-07-array-methods/1-forEach-map.js
@@ -25,3 +25,20 @@ const dollarConverted = dollar.map((value) => {
 
 console.log(dollar);
 console.log(dollarConverted);
+
+const rectangles = [
+    {width: 4, height: 5},
+    {width: 7, height: 10},
+    {width: 14, height: 7},
+]
+
+const calculateAreas = rectangles.map((value, index) => {
+   return {
+    name: `Retângulo ${index + 1}`,
+    area: value.width * value.height,
+   }; 
+});
+
+console.log(calculateAreas);
+console.table(calculateAreas);
+document.write(JSON.stringify(calculateAreas));
