a668bf113980e919bd4e20f51328749022bc93ae
Eric Batista <batistaeric62@gmail.com>
2023-11-17 20:33:55 -0300

    update oop

--- 4-JavaScript/class-11-oop/1-class.js
+++ 4-JavaScript/class-11-oop/1-class.js
@@ -4,24 +4,71 @@
 // entidades do mundo real.
 // This (Scope)
 
-class Brush {
-    constructor(color, height) {
-        this.colorName = color;
-        this.height = height
+// class Brush {
+//     constructor(color, height) {
+//         this.colorName = color;
+//         this.height = height
+//     }
+
+//     action() {
+//         console.log("Escreve");
+//     }
+// }
+
+// const blueBrush = new Brush("Azul", 10);
+// const redBrush = new Brush("Vermelho", 12);
+
+// console.log(blueBrush.colorName);
+// console.log(redBrush.colorName);
+
+// console.log(blueBrush.height);
+// console.log(redBrush.height);
+
+// redBrush.action();
+
+class Cavaleiro {
+    constructor(name, armor) {
+        this.name = name;
+        this.armor = armor;
+        this.health = 100;
     }
 
-    action() {
-        console.log("Escreve");
+    attack() {
+        console.log(`${this.name} atacou!`);
+    }
+
+    damage(effective, damage = 20) {
+        if (effective) {
+            this.health -= damage * 1.3;
+            // console.log(`${this.name} sofreu`);
+        } else {
+            this.health -= damage;
+        }
     }
 }
 
-const blueBrush = new Brush("Azul", 10);
-const redBrush = new Brush("Vermelho", 12);
+const Shun = new Cavaleiro("Shun", "Andromeda")
+const Shiryu = new Cavaleiro("Shiryu", "Dragão")
+const Seiya = new Cavaleiro("Seiya", "Pegasus")
+const Hyoga = new Cavaleiro("Hyoga", "Cisne")
+const Ikki = new Cavaleiro("Ikki", "Fenix")
 
-console.log(blueBrush.colorName);
-console.log(redBrush.colorName);
+console.log(Shun.name);
+console.log(Shun.armor);
+console.log(Ikki.armor);
+console.log(Ikki.health);
 
-console.log(blueBrush.height);
-console.log(redBrush.height);
+Ikki.attack();
 
-redBrush.action();
+console.log(Hyoga.name);
+console.log(Hyoga.health);
+
+Hyoga.damage();
+console.log(Hyoga.health);
+
+Hyoga.damage();
+console.log(Hyoga.health);
+
+const effective = true
+Hyoga.damage(effective);
+console.log(Hyoga.health);
