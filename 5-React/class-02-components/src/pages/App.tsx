65383a0b2abb243751f0bd4f0285aa10b883a069
Eric Batista <batistaeric62@gmail.com>
2023-12-06 20:33:43 -0300

    add card profile

---
+++ 5-React/class-02-components/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { CardProfile } from "../components/CardProfile";
+
+export function App() {
+  return (
+    <>
+      <h1>Class 02 Components</h1>
+      <CardProfile name={"Eric"} age={19}/>
+      <CardProfile name={"Dante"} age={2000}/>
+    </>
+  );
+}
