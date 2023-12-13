47b9379014b3965d9d95eea04ac1f1bb690c4913
Eric Batista <batistaeric62@gmail.com>
2023-12-13 20:53:02 -0300

    add class css in js

---
+++ 5-React/class-05-css-in-js/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/Button";
+import { AppContainer } from "./styles";
+
+export function App() {
+  return (
+    <AppContainer>
+      <h1>Class 05 CSS in JS</h1>
+      <Button/>
+    </AppContainer>
+  );
+}
